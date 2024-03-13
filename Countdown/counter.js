27ccf2fa51e7b3551e1240d06cc46725acd75de3
LeafyLettuce <marci@levelei.com>
2024-03-13 20:28:15 +0100

    Countdown Beta 2

---
+++ Countdown/counter.js
@@ -0,0 +1,101 @@
+function roundToDecimal(number, decimals){
+    number = Math.floor(number*(10**decimals))/(10**decimals)
+    if (String(number).includes('.') == false){
+        return String(number) + "." + "0".repeat(decimals)
+    }
+    else{
+        return String(number) + "0".repeat(decimals-(String(number).split('.')[1].length))
+    }
+}
+
+/// Define the refresh function
+function refresh(){
+    /// Define base variables
+    var left = end - Date.now()
+    var seconds = Math.floor(left/1000)
+    var minutes = Math.floor(seconds/60)
+    var hours = Math.floor(minutes/60)
+    var days = Math.floor(hours/24)
+
+    /// Display base variables
+    document.getElementById("clock_days").textContent = String(days);
+    document.getElementById("clock_hours").textContent = String(hours%24);
+    document.getElementById("clock_mins").textContent = String(minutes%60);
+    document.getElementById("clock_secs").textContent = String(seconds%60);
+
+    /// Define and display alternative unit
+    switch (document.getElementById("clock_alter_unit").value){
+        case "miliseconds": 
+        document.getElementById("clock_alter_value").textContent = left;
+        break
+
+        case "seconds": 
+        document.getElementById("clock_alter_value").textContent = String(Math.floor(left/1000));
+        break
+
+        case "minutes": 
+        document.getElementById("clock_alter_value").textContent = roundToDecimal(left/1000/60, 1);
+        break
+
+        case "hours": 
+        document.getElementById("clock_alter_value").textContent = roundToDecimal(left/1000/60/60, 3);
+        break
+
+        case "days": 
+        document.getElementById("clock_alter_value").textContent = roundToDecimal(left/1000/60/60/24, 4);
+        break
+
+        case "weeks": 
+        document.getElementById("clock_alter_value").textContent = roundToDecimal(left/1000/60/60/24/7, 5);
+        break
+
+        case "months": 
+        document.getElementById("clock_alter_value").textContent = roundToDecimal(left/1000/60/60/24/30, 6);
+        break
+
+        case "years": 
+        document.getElementById("clock_alter_value").textContent = roundToDecimal(left/1000/60/60/24/356.25, 7);
+        break
+
+        case "percent": 
+        document.getElementById("clock_alter_value").textContent = roundToDecimal((1-(end - Date.now())/(end-start))*100, 4) +" %";
+        break
+    }
+    
+    
+    /// Adjust bar
+    if (Date.now > end){
+        document.getElementById("bar").style.width = "100%"
+        document.getElementById("bar").textContent = "100%"
+    
+    }
+    else{
+        document.getElementById("bar").style.width = String(Math.floor((1-(end - Date.now())/(end-start))*100))+"%"
+        document.getElementById("bar").textContent = String(Math.floor((1-(end - Date.now())/(end-start))*100))+"%"
+    }    
+}
+
+/// Initiate refresh
+if (Date.now() < start){
+    gotoStartPage()
+}
+else if (Date.now > end){
+    gotoEndPage()
+}
+
+refresh()
+var intervalId = window.setInterval(function(){
+    refresh()
+}, 7);
+
+/// Switch Handler
+function switchOnclickHandler(){
+    if (document.getElementById("unitswitch").checked){
+        document.getElementById("clockface").style.display = "none"
+        document.getElementById("clockface_alter").style.display = "flex"
+    }
+    else{
+        document.getElementById("clockface").style.display = "flex"
+        document.getElementById("clockface_alter").style.display = "none"
+    }
+}
--- Countdown/language.js
+++ Countdown/language.js
@@ -1,9 +1,7 @@
 function setLanguage(lan){
     switch (lan){
-        case null:
-            break
-        
         case "de":
+            document.title = "Countdown Zähler"
             document.getElementById("toptext").textContent = "Übrige Zeit:"
             document.getElementById("clock_units_days").textContent = "Tage,"
             document.getElementById("clock_units_hours").textContent = "Stunden,"
@@ -20,8 +18,10 @@ function setLanguage(lan){
             document.getElementById("clock_alter_unit").options[6].textContent = "Monaten"
             document.getElementById("clock_alter_unit").options[7].textContent = "Jahre"
             document.getElementById("clock_alter_unit").options[8].textContent = "Prozent"
+            break
     
         case "zh":
+            document.title = "倒数计时器"
             document.getElementById("toptext").textContent = "剩余时间："
             document.getElementById("clock_units_days").textContent = "天,"
             document.getElementById("clock_units_hours").textContent = "小时,"
@@ -38,8 +38,10 @@ function setLanguage(lan){
             document.getElementById("clock_alter_unit").options[6].textContent = "个月"
             document.getElementById("clock_alter_unit").options[7].textContent = "年"
             document.getElementById("clock_alter_unit").options[8].textContent = "百分"
+            break
     
         case "hu":
+            document.title = "Visszaszámláló"
             document.getElementById("toptext").textContent = "Hátra lévő idő:"
             document.getElementById("clock_units_days").textContent = "nap,"
             document.getElementById("clock_units_hours").textContent = "óra,"
@@ -56,4 +58,25 @@ function setLanguage(lan){
             document.getElementById("clock_alter_unit").options[6].textContent = "hónap"
             document.getElementById("clock_alter_unit").options[7].textContent = "év"
             document.getElementById("clock_alter_unit").options[8].textContent = "százalék"
+            break
+    
+        default:
+            document.title = "Countdown Timer"
+            document.getElementById("toptext").textContent = "Time left:"
+            document.getElementById("clock_units_days").textContent = "days,"
+            document.getElementById("clock_units_hours").textContent = "hours,"
+            document.getElementById("clock_units_mins").textContent = "minutes,"
+            document.getElementById("clock_units_secs").textContent = "seconds"
+            document.getElementById("unit_switch_div_text").textContent = "Single unit mode"
+
+            document.getElementById("clock_alter_unit").options[0].textContent = "miliseconds"
+            document.getElementById("clock_alter_unit").options[1].textContent = "seconds"
+            document.getElementById("clock_alter_unit").options[2].textContent = "minutes"
+            document.getElementById("clock_alter_unit").options[3].textContent = "hours"
+            document.getElementById("clock_alter_unit").options[4].textContent = "days"
+            document.getElementById("clock_alter_unit").options[5].textContent = "weeks"
+            document.getElementById("clock_alter_unit").options[6].textContent = "months"
+            document.getElementById("clock_alter_unit").options[7].textContent = "years"
+            document.getElementById("clock_alter_unit").options[8].textContent = "percent"
+            break
 }}
